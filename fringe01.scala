49656853c5c151348264df2ff886eef7e75775c6
Yusuke Sangenya <longinus.eva@gmail.com>
2017-08-17 00:13:27 +0900

    add comment

--- fringe01.scala
+++ fringe01.scala
@@ -1,12 +1,15 @@
 object Fringe {
 	def main(args: Array[String]): Unit = {
+		// Q1
 		show(Some("hello"))
 		show(None)
 
+		// Q2
 		assert_eq(getFirst(Some("hello")), "h")
 		assert_eq(getFirst(Some("")), "")
 		assert_eq(getFirst(None), "")
 
+		// Q3
 		assert_eq(NumEnglishDictionary.translate(Some(1)), Some("one"))
 		assert_eq(NumEnglishDictionary.translate(Some(3)), None)
 		assert_eq(NumEnglishDictionary.translate(None), None)
