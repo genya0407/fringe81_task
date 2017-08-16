0dfaf25f329019459b528544380aac9cf0e3064d
Yusuke Sangenya <longinus.eva@gmail.com>
2017-08-16 22:59:33 +0900

    create test function

--- fringe01.scala
+++ fringe01.scala
@@ -6,4 +6,11 @@ object Fringe {
 	def show(value: Option[String]): Unit = {
 		value.map(println(_))
 	}
+	def assert_eq[Eq](a: Eq, b: Eq): Unit = {
+		if (a == b) {
+			print(".")
+		} else {
+			throw new Exception(s"`$a` is not `$b`")
+		}
+	}
 }
