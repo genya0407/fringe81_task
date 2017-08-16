a0ed73486e3d1298fc57d2e3519094a90fcdfef3
Yusuke Sangenya <longinus.eva@gmail.com>
2017-08-16 22:59:41 +0900

    problem 2

--- fringe01.scala
+++ fringe01.scala
@@ -2,10 +2,20 @@ object Fringe {
 	def main(args: Array[String]): Unit = {
 		show(Some("hello"))
 		show(None)
+
+		assert_eq(getFirst(Some("hello")), "h")
+		assert_eq(getFirst(Some("")), "")
+		assert_eq(getFirst(None), "")
 	}
+
 	def show(value: Option[String]): Unit = {
 		value.map(println(_))
 	}
+
+	def getFirst(value: Option[String]): String = {
+		value.flatMap(_.headOption).mkString
+	}
+
 	def assert_eq[Eq](a: Eq, b: Eq): Unit = {
 		if (a == b) {
 			print(".")
