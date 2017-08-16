7ad1434ccd36f6826d8b77e90225f7ea75adb058
Yusuke Sangenya <longinus.eva@gmail.com>
2017-08-17 00:13:38 +0900

    problem 5

--- fringe01.scala
+++ fringe01.scala
@@ -13,6 +13,16 @@ object Fringe {
 		assert_eq(NumEnglishDictionary.translate(Some(1)), Some("one"))
 		assert_eq(NumEnglishDictionary.translate(Some(3)), None)
 		assert_eq(NumEnglishDictionary.translate(None), None)
+
+		// Q5
+		val scores = Map(
+			"ichiro" -> Map("math"->82, "english"->99),
+			"jiro" -> Map("japanese"->100, "social"->88),
+			"saburo" -> Map("math"->76, "english"->80),
+			"shiro" -> Map("math" -> 99, "social" -> 81),
+			"hanako" -> Map("math"->84, "english"->78, "social"->66))
+		val expected = Map("ichiro" -> 90, "hanako" -> 81)
+		assert_eq(passStudents(scores), expected)
 	}
 
 	def show(value: Option[String]): Unit = {
@@ -21,6 +31,16 @@ object Fringe {
 
 	def getFirst(value: Option[String]): String = {
 		value.flatMap(_.headOption).mkString
+	}
+
+	def passStudents(scores: Map[String, Map[String, Int]]): Map[String, Int] = {
+		scores.mapValues { subjects =>
+			val mEnglish = subjects.get("english")
+			val mMath = subjects.get("math")
+			mEnglish.flatMap(english => mMath.map(math => (english + math)/2))
+		}.collect {
+			case (student, Some(score)) if score >= 80 => student -> score
+		}
 	}
 
 	def assert_eq[Eq](a: Eq, b: Eq): Unit = {
